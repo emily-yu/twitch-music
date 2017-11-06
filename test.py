f18310a6bce08bd5c858b7cf55f75cc2becbd19c
ey1031 <eyudeveloper@gmail.com>
2017-11-05 20:07:29 -0800

    iterating over all the possible lyrics

--- test.py
+++ test.py
@@ -17,13 +17,51 @@ from threading import Thread, Lock, Event
 
 from TwitchChat import TwitchChat
 from Youtube import VideoStats
+from animelyrics import AnimeLyrics
+
+from itertools import product
+import itertools
 
 # stop = Event()
 # tc = TwitchChat(stop, "oxstormthunder")
 # tc.start();
 # tc.stop()
+def google(keyword):
+	# keyword = request.args.get("key")
+	resultArray = []
+	al = AnimeLyrics(keyword)
+	resultArray = al.lyrics("jp")
 
-ytv = VideoStats("https://www.youtube.com/watch?v=m8MfJg68oCs")
+	return (resultArray)
+
+def isEnglish(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+
+ytv = VideoStats("https://www.youtube.com/watch?v=4GPKYLabHv0")
 print(ytv.title())
-print(ytv.duration())
-ytv.open()
+permutation = ytv.title().split()
+print(permutation)
+for word in permutation:
+	if (isEnglish(word) == False):
+		permutation.remove(word)
+
+keywords = [''.join(i) for i in itertools.product(permutation, repeat = 3)]
+
+for key in keywords:
+	print(key)
+	try: 
+		print(google(key + "lyrics"))
+	except AttributeError:
+		print("none")
+	else:
+		# break;
+		print("same")
+
+
+# print(ytv.duration())
+# ytv.open()
