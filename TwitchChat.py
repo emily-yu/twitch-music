d33fa34108c6774787e2e0d2d81b469a8b1c9504
ey1031 <eyudeveloper@gmail.com>
2018-01-11 13:05:23 -0800

    filter out non-youtube links and style

--- TwitchChat.py
+++ TwitchChat.py
@@ -4,10 +4,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
 
 import threading
 from threading import Thread, Lock, Event
 import re
+
+from Youtube import VideoStats
 
 class TwitchChat(Thread):
 
@@ -22,7 +25,7 @@ class TwitchChat(Thread):
         node = twitchDriver.find_element_by_xpath("/html/body")
         script = "arguments[0].insertAdjacentHTML('afterEnd', arguments[1])"
         twitchDriver.execute_script(script, node, "<div class = 'music-queuer'>\
-                                                        <p class = 'music-queuer-header' style = 'position: absolute;top: 20px;right: 20px;'>Music Queue</p>\
+                                                        <p class = 'music-queuer-header' style = 'position: absolute;top: 15px;right: 20px;font-weight: bold'>MUSIC QUEUE</p>\
                                                     </div>")
         # self.addSong("test", twitchDriver)
         # self.addSong("test2", twitchDriver)
@@ -31,14 +34,19 @@ class TwitchChat(Thread):
     def addSong(self, text, twitchDriver):
         node2 = twitchDriver.find_elements_by_xpath("/html/body/div[@class='music-queuer']/p")
         if len(node2) > 1:
-            topStyle = int(node2[1].value_of_css_property("top")[:-2]) + 20
+            topStyle = int(node2[1].value_of_css_property("top")[:-2]) + 35
+        elif len(node2) == 1:
+            topStyle = int(node2[0].value_of_css_property("top")[:-2]) + 65
         else: 
-            topStyle = int(node2[0].value_of_css_property("top")[:-2]) + 20
+            topStyle = int(node2[0].value_of_css_property("top")[:-2]) + 35
 
         node = twitchDriver.find_element_by_xpath("/html/body/div/p[@class='music-queuer-header']")
         script = "arguments[0].insertAdjacentHTML('afterEnd', arguments[1])"
 
-        twitchDriver.execute_script(script, node, "<p style = 'position: absolute; top: " + str(topStyle) + "px; right: 20px'>" + text + "</p>")
+        ytv = VideoStats(text)
+
+        twitchDriver.execute_script(script, node, "<h6 style = 'position: absolute; right: 20px; top: " + str(topStyle - 13) + "px'>" + ytv.title() + "</h6>\
+                                                   <p style = 'position: absolute; top: " + str(topStyle) + "px; right: 20px'>" + text + "</p>")
 
     # retrieve element messages from chat
     def findMesssages(self, html, twitchDriver):
@@ -50,7 +58,7 @@ class TwitchChat(Thread):
         # adding and checking for songs to add to queue
         for divs in mydivs:
             print(divs.get_text().strip())
-            if len((re.findall(r'(https?://[^\s]+)', divs.get_text().strip()))) != 0:
+            if len((re.findall(r'(https?://[^\s]+)', divs.get_text().strip()))) != 0 and "youtube.com/watch?" in divs.get_text().strip():
                 print("FOUND")
                 print()
                 try:
@@ -70,6 +78,9 @@ class TwitchChat(Thread):
 
         wait = WebDriverWait(twitchDriver, 100)
         h3 = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "body.ember-application")))
+
+        youtubeDriver = webdriver.Chrome()
+        youtubeDriver.get("https://www.youtube.com/")
 
         # start loop
         self.injectUI(twitchDriver)
